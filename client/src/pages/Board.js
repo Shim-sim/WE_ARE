82559ee491e7f11d35695791e888bfe9b5112a1f
Shim-sim <shimdb96@gmail.com>
2022-09-10 07:23:46 +0000

    Feat: BoardHeader 검색 창 추가

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -1,27 +1,47 @@
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { logoutUser } from '../_actions/user_action'
 import { useNavigate } from 'react-router-dom'
+import { ListDiv } from '../components/Board/BoardStyle'
+import { DropdownButton, Dropdown } from "react-bootstrap";
+
+
+import StyledContainer from '../components/Style/styledContainer'
 import Auth from '../hoc/auth'
 
 function Board() {
 	const dispatch = useDispatch()
 	const navigate = useNavigate()
 	
-	const onClick = () => {
-		dispatch(logoutUser())
-		.then(response => {
-			if(response.payload.logoutSuccess) {
-				navigate('/')
-			}
-		})
-	}
+	const [sort, setSort] = useState('최신순')
+	
+
 	
 	return (
-		<div style={{top: '50%', position:'absolute'}}>
-			Board 페이지 로그인 된 회원들만 지금 들어옴
-			<br />
-			<button onClick={onClick}>로그아웃 버튼</button>
-		</div>
+		<>
+			<ListDiv>
+				<div className="list__Option">
+				<form>
+				<input 
+					type="text"
+					placeholder="제목, 내용 검색"
+				/>
+					<button>검색</button>
+				</form>
+				<DropdownButton align="end" variant="outline-secondary" title={sort} >
+          <Dropdown.Item onClick={()=> setSort('최신순')}>
+            최신순
+          </Dropdown.Item>
+          <Dropdown.Item onClick={()=> setSort('인기순')}>
+            인기순
+          </Dropdown.Item>
+        </DropdownButton>
+				</div>
+			</ListDiv>
+			<StyledContainer >
+
+			</StyledContainer>
+			</>
 		
 	)
 }
