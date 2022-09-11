99c4b907fbeb47e77eaf3f14aa1b84eec3694963
Shim-sim <shimdb96@gmail.com>
2022-09-11 13:21:01 +0000

    Feat: 자신의 글 삭제 기능 추가

---
+++ client/src/components/Board/DeleteBoard.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import styled from 'styled-components'
+import { USER_SERVER } from '../Config.js'
+
+
+const Button = styled.button`
+	color: #c62912;
+  font-size: 12px;
+  line-height: 22px;
+`
+
+
+
+export default function DeleteBoard(props) {
+	const onDelete = () => {
+		
+		let variables = {
+			boardFrom: props.board,
+			userFrom: props.user
+		}
+		let comfirmDelete = window.confirm('삭제하시겠습니까?')
+		comfirmDelete && axios.post(`${USER_SERVER}/board/deleteBoard`, variables)
+		.then(response => {
+			if(response.data.success) {
+				alert('게시글 삭제에 성공했습니다.')
+				props.onRemove(response.data.result._id)
+			} else {
+				alert('게시글 삭제에 실패했습니다.')
+			}
+		})
+	}
+	
+	return (
+		<Button onClick={onDelete}>
+			삭제
+		</Button>
+	)
+}
