148d20ccfb5637b4cba33d72f5bfad62bb88a43d
Shim-sim <shimdb96@gmail.com>
2022-09-12 04:46:34 +0000

    Design: 코멘트, 버튼 완성

---
+++ client/src/components/Board/CommentInput.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background-color: transparent;
+  width: 100%;
+  height: 40px;
+  padding: 0 10px;
+  font-size: 13px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+	border-radius: 15px;
+  box-sizing: border-box;
+`
+
+const CommentInput = function({name, placeholder, value, onChange}) {
+  return (
+    <Input
+      name={name}
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+    />
+  );   
+}
+
+export default CommentInput;
