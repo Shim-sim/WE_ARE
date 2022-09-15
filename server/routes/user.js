d5476f6bdb888b17c8d6300280e6806df1f1998d
Shim-sim <shimdb96@gmail.com>
2022-09-15 11:17:16 +0000

    Feat: 회원탈퇴 기능 완성

--- server/routes/user.js
+++ server/routes/user.js
@@ -34,11 +34,46 @@ router.get('/profile', auth, (req, res) => {
 
 
 router.post('/myBoard', (req, res) => {
-	Board.findOne({ userFrom : req.body.userFrom })
+	Board.find({ userFrom : req.body.userFrom })
 		.sort({ createdAt: -1})
 		.exec((err, boards) => {
 			if(err) return res.status(400).send(err)
 			return res.status(200).json({ success: true, boards})
+	})
+})
+
+
+router.post('/update/nickname', auth, (req, res) => {
+		User.findOneAndUpdate(
+			{_id: req.body._id},
+			{$set:{nickname: req.body.nickname}},
+			{new: true},
+			(err, user) => {
+				if(user) return res.status(200).send()
+				else return res.status(404).send
+			}
+		)
+})
+
+router.post('/withdrawal', auth, (req, res) => {
+	User.findOne({ _id: req.body._id }, (err, user) => {
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if(!isMatch) return res.json({ success: false, message: '비밀번호가 틀렸습니다.' })
+			else {
+				Comment.deleteMany({ userFrom: user._id })
+					.exec((err, result) => {
+						return { success: true, result }
+				})
+				Board.deleteMany({ userFrom: user._id })
+					.exec((err, result) => {
+						return { success: true, result }
+				})
+				User.deleteOne({ _id: req.body._id }, (err, user) => {
+					if(err) return res.status(400).send();
+					else return res.json({ success: true })
+				})
+			}
+		})
 	})
 })
 
