26e08b010866e582e9e5a0057348f613cd2816a1
Shim-sim <shimdb96@gmail.com>
2022-09-24 14:57:45 +0000

    Feat: router 변경

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,3 +1,3 @@
-//SERVER ROUTES 구름
+//SERVER ROUTES 구름 'https://we-are-server.run.goorm.io'
 
-export const USER_SERVER = 'https://we-are-server.run.goorm.io'
+export const USER_SERVER = 'https://we-are-app.herokuapp.com'
