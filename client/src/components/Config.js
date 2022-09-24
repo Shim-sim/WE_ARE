10b9d90cb8311731e546ffca1d4b75aeb2b79b97
Shim-sim <shimdb96@gmail.com>
2022-09-24 13:05:20 +0000

    Fix: config

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,6 +1,6 @@
 //SERVER ROUTES 구름
 
-export const USER_SERVER = 'https://we-are-app.herokuapp.com/'
+export const USER_SERVER = 'https://we-are-app.herokuapp.com'
 
 
-export const LOCAL_SERVER = "https://we-are-app.herokuapp.com/"
+export const LOCAL_SERVER = "https://we-are-app.herokuapp.com"
