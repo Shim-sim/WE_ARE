ab01e89026e2f2c236049eb74e34d0c07c7ad40c
Shim-sim <shimdb96@gmail.com>
2022-10-20 07:45:11 +0000

    feat: cors 해결

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors({
-	origin: 'https://we-are-client.run.goorm.io',
+	origin: 'https://we-are-app.herokuapp.com',
 	credentials: true
 }))
 
