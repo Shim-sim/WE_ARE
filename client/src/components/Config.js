e2b9bbe47b0a006b538da4e5f328823d28e3cf4d
ShimSungBo <shimdb96@gmail.com>
2023-01-20 13:32:50 +0900

    refactor: 로컬로 주소 변경 및 proxy설정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,3 +1,4 @@
 //SERVER ROUTES 구름 'https://we-are-server.run.goorm.io'
 // HEROKU ROUTER = 'https://we-are-app.herokuapp.com'
-export const USER_SERVER = 'https://we-are-app.herokuapp.com'
+//
+export const USER_SERVER = 'http://localhost:8000';
--- server/index.js
+++ server/index.js
@@ -1,65 +1,52 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const app = express()
-const http = require('http')
-const path = require("path")
-const config = require('./config/key')
-const registerRouter = require('./routes/register')
-const loginRouter = require('./routes/login')
-const logoutRouter = require('./routes/logout')
-const userRouter = require('./routes/user')
-const boardRouter = require('./routes/board')
-const commentRouter = require('./routes/comment')
-const authRouter = require('./routes/auth')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const path = require('path');
+const config = require('./config/key');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
+const userRouter = require('./routes/user');
+const boardRouter = require('./routes/board');
+const commentRouter = require('./routes/comment');
+const authRouter = require('./routes/auth');
 
-
-
-const mongoose = require('mongoose')
- mongoose.connect(config.mongoURI)
-	.then(() => console.log('몽고db 잘연결됨'))
-	.catch(err => console.log(err));
-
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI)
+  .then(() => console.log('몽고db 잘연결됨'))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors({
-	origin: 'https://we-are-app.herokuapp.com',
-	credentials: true
-}))
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
-if(process.env.NODE_ENV === "production") {
-	app.use(express.static('client/build'));
-	
-	app.get("/", (req, res) => {
-		res.sendFile(path.resolve(__dirname,"../client", "build", "index.html"))
-	});
-	
-	setInterval(() => {
-		http.get('https://we-are-app.herokuapp.com');
-	}, 1200000)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
 }
 
-
-
-
-app.use('/register', registerRouter)
-app.use('/login', loginRouter)
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
-app.use('/user', userRouter)
+app.use('/user', userRouter);
 app.use('/board', boardRouter);
-app.use('/comment', commentRouter)
+app.use('/comment', commentRouter);
 app.use('/auth', authRouter);
 
-
-
-
-
-
-
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
